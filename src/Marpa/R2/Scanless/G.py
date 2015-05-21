87f87218e43d3dbde913ca7b8c27a4d270eb981a
srathbun <spencerrathbun@gmail.com>
2015-05-21 16:58:03 -0400

    Converted perl code to python for Scanless::G from SLG.pm

--- src/Marpa/R2/Scanless/G.py
+++ src/Marpa/R2/Scanless/G.py
@@ -1,316 +1,243 @@
 # Copyright 2015 Jeffrey Kegler
-# This file is part of Marpa::R2.  Marpa::R2 is free software: you can
+# This file is part of Marpa.R2.  Marpa.R2 is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Lesser
 # General Public License as published by the Free Software Foundation,
 # either version 3 of the License, or (at your option) any later version.
 #
-# Marpa::R2 is distributed in the hope that it will be useful,
+# Marpa.R2 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser
-# General Public License along with Marpa::R2.  If not, see
+# General Public License along with Marpa.R2.  If not, see
 # http://www.gnu.org/licenses/.
 
-# package Marpa::R2::Scanless::G;
+# package Marpa.R2.Scanless.G
+
+import Internal.Scanless.G
 
 VERSION = '3.001_000'
 
-sub Marpa::R2::Scanless::G::new {
-    my ( $class, @hash_ref_args ) = @_;
+def new(class, hash_ref_args):
+    slg = []
 
-    my $slg = [];
-    bless $slg, $class;
+    dsl, g1_args = Internal.Scanless.G.set(slg, 'new', hash_ref_args)
+    ast = Internal.MetaAST.new(dsl)
+    hashed_ast = ast.ast_to_hash()
+    Internal.Scanless.G.hash_to_runtime(slg, hashed_ast, g1_args)
+    return slg
 
-    my ($dsl, $g1_args) = Marpa::R2::Internal::Scanless::G::set ( $slg, 'new', @hash_ref_args );
-    my $ast = Marpa::R2::Internal::MetaAST->new( $dsl );
-    my $hashed_ast = $ast->ast_to_hash();
-    Marpa::R2::Internal::Scanless::G::hash_to_runtime($slg, $hashed_ast, $g1_args);
-    return $slg;
-} ## end sub Marpa::R2::Scanless::G::new
+def set(slg, hash_ref_args):
+    Internal.Scanless.G.set(slg, 'set', hash_ref_args)
+    return slg
 
-sub Marpa::R2::Scanless::G::set {
-    my ( $slg, @hash_ref_args ) = @_;
-    Marpa::R2::Internal::Scanless::G::set ( $slg, 'set', @hash_ref_args );
-    return $slg;
-}
-
-sub thick_subgrammar_by_name {
-    my ( $slg, $subgrammar ) = @_;
-
+def thick_defgrammar_by_name(slg, defgrammar='G1'):
     # Allow G0 as legacy synonym for L0
-    state $grammar_names = { 'G0' => 1, 'G1' => 1, 'L0' => 1 };
-    $subgrammar //= 'G1';
+    grammar_names = { 'G0' => 1, 'G1' => 1, 'L0' => 1 }
 
-    Marpa::R2::exception(qq{No lexer named "$subgrammar"})
-        if not defined $grammar_names->{$subgrammar};
+	if not grammar_names[defgrammar]:
+		raise Exception('No lexer named "{0}"'.format(defgrammar))
 
-    return $slg->[Marpa::R2::Internal::Scanless::G::THICK_G1_GRAMMAR]
-        if $subgrammar eq 'G1';
+	if defgrammar == 'G1':
+		return slg[Internal.Scanless.G.THICK_G1_GRAMMAR]
+	else:
+		return slg[Internal.Scanless.G.THICK_LEX_GRAMMARS][0]
 
-    return $slg->[Marpa::R2::Internal::Scanless::G::THICK_LEX_GRAMMARS]
-        ->[0];
-} ## end sub thick_subgrammar_by_name
+def start_symbol_id(slg, rule_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).start_symbol()
 
-sub Marpa::R2::Scanless::G::start_symbol_id {
-    my ( $slg, $rule_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name( $slg, $subgrammar )->start_symbol();
-}
+def rule_name(slg, rule_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).rule_name(rule_id)
 
-sub Marpa::R2::Scanless::G::rule_name {
-    my ( $slg, $rule_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name( $slg, $subgrammar )->rule_name($rule_id);
-}
+def rule_expand(slg, rule_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).tracer()
+        .rule_expand(rule_id)
 
-sub Marpa::R2::Scanless::G::rule_expand {
-    my ( $slg, $rule_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name( $slg, $subgrammar )->tracer()
-        ->rule_expand($rule_id);
-}
+def symbol_name(slg, symbol_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).tracer()
+        .symbol_name(symbol_id)
 
-sub Marpa::R2::Scanless::G::symbol_name {
-    my ( $slg, $symbol_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name($slg, $subgrammar)->tracer()
-        ->symbol_name($symbol_id);
-}
+def symbol_display_form(slg, symbol_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar)
+        .symbol_in_display_form(symbol_id)
 
-sub Marpa::R2::Scanless::G::symbol_display_form {
-    my ( $slg, $symbol_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name( $slg, $subgrammar )
-        ->symbol_in_display_form($symbol_id);
-}
+def symbol_dsl_form(slg, symbol_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar)
+        .symbol_dsl_form(symbol_id)
 
-sub Marpa::R2::Scanless::G::symbol_dsl_form {
-    my ( $slg, $symbol_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name( $slg, $subgrammar )
-        ->symbol_dsl_form($symbol_id);
-}
+def symbol_description(slg, symbol_id, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar)
+        .symbol_description(symbol_id)
 
-sub Marpa::R2::Scanless::G::symbol_description {
-    my ( $slg, $symbol_id, $subgrammar ) = @_;
-    return thick_subgrammar_by_name($slg, $subgrammar)
-        ->symbol_description($symbol_id);
-}
+def rule_show(slg, rule_id, defgrammar):
+    return slg_rule_show(slg, rule_id, thick_defgrammar_by_name(slg, defgrammar))
 
-sub Marpa::R2::Scanless::G::rule_show
-{
-    my ( $slg, $rule_id, $subgrammar) = @_;
-    return slg_rule_show($slg, $rule_id, thick_subgrammar_by_name($slg, $subgrammar));
-}
+def slg_rule_show(slg, rule_id, defgrammar):
+    tracer       = defgrammar.tracer()
+    defgrammar_c = defgrammar[Internal.Grammar.C]
+    symbol_ids   = tracer.rule_expand(rule_id)
+	if isinstance(symbol_ids, basestring):
+		return symbol_ids
+    rhs = map(defgrammar.symbol_in_display_form, symbol_ids)
+	lhs = rhs[0]
+	rhs = rhs[1:]
+    minimum    = defgrammar_c.sequence_min(rule_id)
+    quantifier = ''
 
-sub slg_rule_show {
-    my ( $slg, $rule_id, $subgrammar ) = @_;
-    my $tracer       = $subgrammar->tracer();
-    my $subgrammar_c = $subgrammar->[Marpa::R2::Internal::Grammar::C];
-    my @symbol_ids   = $tracer->rule_expand($rule_id);
-    return if not scalar @symbol_ids;
-    my ( $lhs, @rhs ) =
-        map { $subgrammar->symbol_in_display_form($_) } @symbol_ids;
-    my $minimum    = $subgrammar_c->sequence_min($rule_id);
-    my @quantifier = ();
+    if minimum:
+        quantifier = '*' if minimum <= 0 else '+'
+    return "".join([lhs, '.=', rhs, quantifier])
 
-    if ( defined $minimum ) {
-        @quantifier = ( $minimum <= 0 ? q{*} : q{+} );
-    }
-    return join q{ }, $lhs, q{::=}, @rhs, @quantifier;
-} ## end sub slg_rule_show
+def show_rules(slg, verbose=0, defgrammar='G1'):
+    text = ""
+    thick_grammar = thick_defgrammar_by_name(slg, defgrammar)
+    rules     = thick_grammar[Internal.Grammar.RULES]
+    grammar_c = thick_grammar[Internal.Grammar.C]
 
-sub Marpa::R2::Scanless::G::show_rules {
-    my ( $slg, $verbose, $subgrammar ) = @_;
-    my $text     = q{};
-    $verbose    //= 0;
-    $subgrammar //= 'G1';
+	for rule in rules:
+        rule_id = rule[Internal.Rule.ID]
 
-    my $thick_grammar = thick_subgrammar_by_name($slg, $subgrammar);
+        minimum = grammar_c.sequence_min(rule_id)
+		if minimum:
+			quantifier = '*' if minimum <= 0 else '+'
+		else:
+			quantifier = ''
+        lhs_id      = grammar_c.rule_lhs(rule_id)
+        rule_length = grammar_c.rule_length(rule_id)
+        rhs_ids = []
+		for r in range(0, len(rule_length)-1):
+            rhs_ids.push(grammar_c.rule_rhs(rule_id, r))
 
-    my $rules     = $thick_grammar->[Marpa::R2::Internal::Grammar::RULES];
-    my $grammar_c = $thick_grammar->[Marpa::R2::Internal::Grammar::C];
+        text = text + ' '.join([defgrammar, "Rrule_id",
+            thick_grammar.symbol_in_display_form(lhs_id),
+            '.=',
+            map(thick_grammar.symbol_in_display_form, rhs_ids),
+            quantifier])
+        text = text + "\n"
 
-    for my $rule ( @{$rules} ) {
-        my $rule_id = $rule->[Marpa::R2::Internal::Rule::ID];
+        if verbose >= 2:
+            description = rule[Internal.Rule.DESCRIPTION]
+			if description:
+				text = text + "  description\n"
+            comment = []
+			if grammar_c.rule_length(rule_id) == 0:
+                comment.push('empty')
+			if not thick_grammar.rule_is_used(rule_id):
+                comment.push('!used')
+			if not grammar_c.rule_is_productive(rule_id):
+                comment.push('unproductive')
+			if not grammar_c.rule_is_accessible(rule_id):
+				comment.push('inaccessible')
+			if rule[Internal.Rule.DISCARD_SEPARATION]:
+                comment.push('discard_sep')
 
-        my $minimum = $grammar_c->sequence_min($rule_id);
-        my @quantifier =
-            defined $minimum ? $minimum <= 0 ? (q{*}) : (q{+}) : ();
-        my $lhs_id      = $grammar_c->rule_lhs($rule_id);
-        my $rule_length = $grammar_c->rule_length($rule_id);
-        my @rhs_ids =
-            map { $grammar_c->rule_rhs( $rule_id, $_ ) }
-            ( 0 .. $rule_length - 1 );
-        $text .= join q{ }, $subgrammar, "R$rule_id",
-            $thick_grammar->symbol_in_display_form($lhs_id),
-            '::=',
-            ( map { $thick_grammar->symbol_in_display_form($_) } @rhs_ids ),
-            @quantifier;
-        $text .= "\n";
+			if comment:
+                text = text +
+					' ' + ' '.join('/*', comment, '*/') + "\n"
 
-        if ( $verbose >= 2 ) {
+            text = text + "  Symbol IDs: <lhs_id> .= " +
+				' '.join(map(lambda x: "<" + x +">", rhs_ids)) + "\n"
 
-            my $description = $rule->[Marpa::R2::Internal::Rule::DESCRIPTION];
-            $text .= "  $description\n" if $description;
-            my @comment = ();
-            $grammar_c->rule_length($rule_id) == 0
-                and push @comment, 'empty';
-            $thick_grammar->rule_is_used($rule_id)
-                or push @comment, '!used';
-            $grammar_c->rule_is_productive($rule_id)
-                or push @comment, 'unproductive';
-            $grammar_c->rule_is_accessible($rule_id)
-                or push @comment, 'inaccessible';
-            $rule->[Marpa::R2::Internal::Rule::DISCARD_SEPARATION]
-                and push @comment, 'discard_sep';
+		if verbose >= 3:
+            tracer = thick_grammar.tracer()
+            text = text + "  Internal symbols: <" +
+                tracer.symbol_name(lhs_id) +
+                '> .= ' + ' '.join(
+					map(lambda x: '<' + tracer.symbol_name(x) + '>', rhs_ids)
+                ) + "\n"
 
-            if (@comment) {
-                $text .= q{  } . ( join q{ }, q{/*}, @comment, q{*/} ) . "\n";
-            }
+    return text
 
-            $text .= "  Symbol IDs: <$lhs_id> ::= "
-                . ( join q{ }, map {"<$_>"} @rhs_ids ) . "\n";
+def show_symbols(slg, verbose=0, defgrammar='G1'):
+	text = ''
 
-        } ## end if ( $verbose >= 2 )
+    thick_grammar = thick_defgrammar_by_name(slg, defgrammar)
 
-        if ( $verbose >= 3 ) {
+    symbols   = thick_grammar[Internal.Grammar.SYMBOLS]
+    grammar_c = thick_grammar[Internal.Grammar.C]
 
-            my $tracer = $thick_grammar->tracer();
+    for symbol in symbols:
+        symbol_id = symbol[Internal.Symbol.ID]
 
-            $text
-                .= "  Internal symbols: <"
-                . $tracer->symbol_name($lhs_id)
-                . q{> ::= }
-                . (
-                join q{ },
-                map { '<' . $tracer->symbol_name($_) . '>' } @rhs_ids
-                ) . "\n";
+        text = text + ' '.join([defgrammar, "Ssymbol_id",
+            thick_grammar.symbol_in_display_form(symbol_id)])
 
-        } ## end if ( $verbose >= 3 )
+        description = symbol[Internal.Symbol.DESCRIPTION]
+		if description:
+            text = text + " -- description"
+        text =  text + "\n"
 
-    } ## end for my $rule ( @{$rules} )
+		if verbose >= 2:
+            tag_list = []
+			if not grammar_c.symbol_is_productive(symbol_id):
+                tag_list.push('unproductive')
+			if not grammar_c.symbol_is_accessible(symbol_id):
+                tag_list.push('inaccessible')
+			if grammar_c.symbol_is_nulling(symbol_id):
+                tag_list.push('nulling')
+			if grammar_c.symbol_is_terminal(symbol_id):
+                tag_list.push('terminal')
 
-    return $text;
-} ## end sub Marpa::R2::Scanless::G::show_rules
+			if tag_list:
+                text = text +
+                    ' ' + ' '.join('/*', tag_list, '*/') + "\n"
 
-sub Marpa::R2::Scanless::G::show_symbols {
-    my ( $slg, $verbose, $subgrammar ) = @_;
-    my $text = q{};
-    $verbose    //= 0;
-    $subgrammar //= 'G1';
+            tracer = thick_grammar.tracer()
+            text = text + "  Internal name: <" +
+                tracer.symbol_name(symbol_id) + ">\n"
 
-    my $thick_grammar = thick_subgrammar_by_name($slg, $subgrammar);
+		if verbose >= 3:
+            dsl_form = symbol[Internal.Symbol.DSL_FORM]
+			if dsl_form:
+				text = text + "SLIF name: {0}\n".format(dsl_form)
 
-    my $symbols   = $thick_grammar->[Marpa::R2::Internal::Grammar::SYMBOLS];
-    my $grammar_c = $thick_grammar->[Marpa::R2::Internal::Grammar::C];
+    return text
 
-    for my $symbol ( @{$symbols} ) {
-        my $symbol_id = $symbol->[Marpa::R2::Internal::Symbol::ID];
+def show_dotted_rule(slg, rule_id, dot_position):
+    grammar =  slg[Internal.Scanless.G.THICK_G1_GRAMMAR]
+    tracer  = grammar.tracer()
+    grammar_c = grammar[Internal.Grammar.C]
+	rhs = map(grammar.symbol_in_display_form, tracer.rule_expand(rule_id))
+	lhs = rhs.pop(0)
+    rhs_length = len(rhs)
 
-        $text .= join q{ }, $subgrammar, "S$symbol_id",
-            $thick_grammar->symbol_in_display_form($symbol_id);
+    minimum = grammar_c.sequence_min(rule_id)
+    quantifier = []
+	if minimum:
+        quantifier = '*' if minimum <= 0 else '+'
+	if dot_position < 0:
+		dot_position = 0
+	if dot_position < rhs_length:
+        rhs[dot_position] = '.'
+        return ''.join([' ', lhs, '.', rhs, quantifier])
+    else:
+        return ''.join([' ', lhs, '.', rhs, quantifier, '.'])
 
-        my $description = $symbol->[Marpa::R2::Internal::Symbol::DESCRIPTION];
-        if ($description) {
-            $text .= " -- $description";
-        }
-        $text .= "\n";
+def rule(slg, args):
+    return slg[Internal.Scanless.G.THICK_G1_GRAMMAR]
+        .rule(args)
 
-        if ( $verbose >= 2 ) {
+def rule_ids(slg, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).rule_ids()
 
-            my @tag_list = ();
-            $grammar_c->symbol_is_productive($symbol_id)
-                or push @tag_list, 'unproductive';
-            $grammar_c->symbol_is_accessible($symbol_id)
-                or push @tag_list, 'inaccessible';
-            $grammar_c->symbol_is_nulling($symbol_id)
-                and push @tag_list, 'nulling';
-            $grammar_c->symbol_is_terminal($symbol_id)
-                and push @tag_list, 'terminal';
+def symbol_ids(slg, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).symbol_ids()
 
-            if (@tag_list) {
-                $text
-                    .= q{  } . ( join q{ }, q{/*}, @tag_list, q{*/} ) . "\n";
-            }
+def g1_rule_ids(slg):
+    return slg.rule_ids()
 
-            my $tracer = $thick_grammar->tracer();
-            $text .= "  Internal name: <"
-                . $tracer->symbol_name($symbol_id) . qq{>\n};
+def g0_rule_ids(slg):
+	return slg.rule_ids('L0')
 
-        } ## end if ( $verbose >= 2 )
-
-        if ( $verbose >= 3 ) {
-
-            my $dsl_form = $symbol->[Marpa::R2::Internal::Symbol::DSL_FORM];
-            if ($dsl_form) { $text .= qq{  SLIF name: $dsl_form\n}; }
-
-        } ## end if ( $verbose >= 3 )
-
-    } ## end for my $symbol ( @{$symbols} )
-
-    return $text;
-} ## end sub Marpa::R2::Scanless::G::show_symbols
-
-sub Marpa::R2::Scanless::G::show_dotted_rule {
-    my ( $slg, $rule_id, $dot_position ) = @_;
-    my $grammar =  $slg->[Marpa::R2::Internal::Scanless::G::THICK_G1_GRAMMAR];
-    my $tracer  = $grammar->tracer();
-    my $grammar_c = $grammar->[Marpa::R2::Internal::Grammar::C];
-    my ( $lhs, @rhs ) =
-    map { $grammar->symbol_in_display_form($_) } $tracer->rule_expand($rule_id);
-    my $rhs_length = scalar @rhs;
-
-    my $minimum = $grammar_c->sequence_min($rule_id);
-    my @quantifier = ();
-    if (defined $minimum) {
-        @quantifier = ($minimum <= 0 ? q{*} : q{+} );
-    }
-    $dot_position = 0 if $dot_position < 0;
-    if ($dot_position < $rhs_length) {
-        splice @rhs, $dot_position, 0, q{.};
-        return join q{ }, $lhs, q{->}, @rhs, @quantifier;
-    } else {
-        return join q{ }, $lhs, q{->}, @rhs, @quantifier, q{.};
-    }
-} ## end sub Marpa::R2::Grammar::show_dotted_rule
-
-sub Marpa::R2::Scanless::G::rule {
-    my ( $slg, @args ) = @_;
-    return $slg->[Marpa::R2::Internal::Scanless::G::THICK_G1_GRAMMAR]
-        ->rule(@args);
-}
-
-sub Marpa::R2::Scanless::G::rule_ids {
-    my ($slg, $subgrammar) = @_;
-    return thick_subgrammar_by_name($slg, $subgrammar)->rule_ids();
-}
-
-sub Marpa::R2::Scanless::G::symbol_ids {
-    my ($slg, $subgrammar) = @_;
-    return thick_subgrammar_by_name($slg, $subgrammar)->symbol_ids();
-}
-
-sub Marpa::R2::Scanless::G::g1_rule_ids {
-    my ($slg) = @_;
-    return $slg->rule_ids();
-}
-
-sub Marpa::R2::Scanless::G::g0_rule_ids {
-    my ($slg) = @_;
-    return $slg->rule_ids('L0');
-}
-
-sub Marpa::R2::Scanless::G::g0_rule {
-    my ( $slg, @args ) = @_;
-    return $slg->[Marpa::R2::Internal::Scanless::G::THICK_LEX_GRAMMARS]->[0]
-        ->rule(@args);
-}
+def g0_rule(slg, args):
+    return slg[Internal.Scanless.G.THICK_LEX_GRAMMARS][0]
+        .rule(args)
 
 # Internal methods, not to be documented
 
-sub Marpa::R2::Scanless::G::thick_g1_grammar {
-    my ($slg) = @_;
-    return $slg->[Marpa::R2::Internal::Scanless::G::THICK_G1_GRAMMAR];
-}
+def thick_g1_grammar(slg):
+    return slg[Internal.Scanless.G.THICK_G1_GRAMMAR]
 
-sub Marpa::R2::Scanless::G::show_irls {
-    my ($slg, $subgrammar) = @_;
-    return thick_subgrammar_by_name($slg, $subgrammar)->show_irls();
-}
+def show_irls(slg, defgrammar):
+    return thick_defgrammar_by_name(slg, defgrammar).show_irls()
